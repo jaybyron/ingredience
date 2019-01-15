bce8ca8e4c06db6766cc572108f57c6152f74e86
Byron Inocencio <jay.byron@gmail.com>
2019-01-15 15:03:23 -0800

    setup our webpack and got react running. added more node modules

---
+++ src/client/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return   <div>
+      <h1> it fucking works</h1>
+    </div>
+  }
+}
+
+export default App;
